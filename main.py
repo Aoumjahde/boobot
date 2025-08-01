322bd271170be5d669320c3ec3a85decc9dbdc04
Aziz Oumjahde <aziz.dev@gmail.com>
2025-08-01 20:35:50 +0100

    Add the counter of words

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+# from stats import get_num_words
 
 
 def get_book_text(filePath):
@@ -7,12 +7,19 @@ def get_book_text(filePath):
         return file_contents
 
 def sptlitingString (textContent):
+    count = 0
     textSpliting = textContent.split()
     print(textSpliting)
+
+    for word in textContent:
+        count +=1
+    print(f"{count} words found in the document")
 
 
 def main():
     textContent = get_book_text("books/frankenstein.txt")
     sptlitingString(textContent)
+
+
 
 main()
