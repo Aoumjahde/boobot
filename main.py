7480b350bf549ea6e9fa1601ef513d3140f654a6
Aziz Oumjahde <aziz.dev@gmail.com>
2025-08-01 20:15:10 +0100

    Solve the text spliting

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("")
+from stats import get_num_words
+
+
+def get_book_text(filePath):
+    with open(filePath, encoding="utf8") as f:
+        file_contents = f.read()
+        return file_contents
+
+def sptlitingString (textContent):
+    textSpliting = textContent.split()
+    print(textSpliting)
+
+
+def main():
+    textContent = get_book_text("books/frankenstein.txt")
+    sptlitingString(textContent)
+
+main()
